7223fda3d9e1d8ec1e6e941177e23d25fe174a8b
Linkon Siddique <lsiddique415@gmail.com>
2020-03-12 01:43:26 +0600

    DB First workflow

---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Post()
+            {
+                PostId = 1,
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                Body = "Body"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Entity-Framework-In-Depth" 
